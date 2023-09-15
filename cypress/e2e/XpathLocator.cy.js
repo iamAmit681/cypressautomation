07c3853b3db03fc51d9d8304e23b58236f82da1d
iamAmit681 <143062291+iamAmit681@users.noreply.github.com>
2023-09-15 17:14:24 +0530

    Delete cypress/e2e/XpathLocator.cy.js

--- cypress/e2e/XpathLocator.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Xpath locator',()=>
-{
-    it('find total product',()=>
-    {
-        cy.visit("http://www.automationpractice.pl/index.php?id_category=3&controller=category")
-        cy.xpath("//ul[@class='product_list grid row']//li").should('have.length',22)
-    })
-})
